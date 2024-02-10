936e592746b017d84cee4b03252fdd6671f9dd86
Abel <abelastra0@gmail.com>
2024-02-10 19:02:14 -0300

    creacion de la pagina dulce fortmula

---
+++ index.js
@@ -0,0 +1,5 @@
+ const hambur = document.querySelector(".nav-hambur")
+  const  navul = document.querySelector(".nav-ul")
+  hambur.addEventListener("click",()=>{
+     navul.classList.toggle("nav-ul_visibl")
+  })
